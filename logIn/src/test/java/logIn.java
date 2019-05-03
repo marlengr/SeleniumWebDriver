d1dada98c9cfce3e19b18030cd89ea2655cb40f1
Marlena <Grabowska.marlena@tlen.pl>
2019-05-03 11:55:33 +0200

    logIn changed

--- logIn/src/test/java/logIn.java
+++ logIn/src/test/java/logIn.java
@@ -4,15 +4,22 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class logIn {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @Before
     public void before(){
         System.setProperty("webdriver.chrome.driver", "C://Users/Kordian/driver/chromedriver.exe");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
         driver.get("http://localhost/litecart/admin/login.php");
     }
 
@@ -21,6 +28,7 @@ public class logIn {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
     }
 
     @After
